6492ce195e6b5cde4ace67e77fd8136c93bf07fe
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-05-24 15:55:37 +0100

    Added Guarda3 method as well as get and set items

---
+++ MyGenericClass/Guarda3.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Guarda3<T>
+{
+    private T[] items = new T[3];
+
+    public Guarda3()
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i] = default(T);
+        }
+    }
+
+    public T GetItem(int i)
+    {
+        if (i < 0 || i > 2)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        return items[i];
+    }
+
+    public void SetItem(int i, T item)
+    {
+        if (i < 0 || i > 2)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        items[i] = item;
+    }
+}
