4d7f3eaa7e5a97b11032fc14ff02798ae6aa605d
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-05-24 15:21:54 +0100

    Added checker class

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HowManyOfThisType
+{
+    public static class Checker
+{
+    public static int HowManyOfType<T>(IEnumerable<object> items)
+    {
+        int count = 0;
+        foreach (var item in items)
+        {
+            if (item is T)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+}
