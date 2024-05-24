b7107ec1756f1434934ac4c08eff067c3c2f056c
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-05-24 16:20:23 +0100

    Re used program from MygenericCLass

--- MyEnumerable/Program.cs
+++ MyEnumerable/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace MyEnumerable
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Guarda3<string> guardaString = new Guarda3<string>();
+        guardaString.SetItem(0, "Hello");
+        guardaString.SetItem(1, "World");
+        guardaString.SetItem(2, "!");
+        Console.WriteLine("Strings in guardaString:");
+        foreach (var item in guardaString)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(item);
+        }
+
+        Guarda3<float> guardaFloat = new Guarda3<float>();
+        guardaFloat.SetItem(0, 1.1f);
+        guardaFloat.SetItem(1, 2.2f);
+        guardaFloat.SetItem(2, 3.3f);
+        Console.WriteLine("\nFloats in guardaFloat:");
+        foreach (var item in guardaFloat)
+        {
+            Console.WriteLine(item);
+        }
+
+        try
+        {
+            guardaFloat.GetItem(3);
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("\nIndex out of range exception caught.");
         }
     }
 }
