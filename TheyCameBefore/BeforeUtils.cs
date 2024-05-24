992ccdf42a6a089ff9967b548f50659dd5d192f5
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-05-24 15:49:10 +0100

    Added BeforeUtils.cs

---
+++ TheyCameBefore/BeforeUtils.cs
@@ -0,0 +1,19 @@
+﻿// BeforeUtils.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class BeforeUtils
+{
+    public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, T item) where T : struct, IComparable<T>
+    {
+        foreach (var currentItem in items)
+        {
+            if (currentItem.CompareTo(item) < 0)
+            {
+                yield return currentItem;
+            }
+        }
+    }
+}
+
